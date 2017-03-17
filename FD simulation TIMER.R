cba45da5a867a345b5e9c90dd4a444b7ba9a9d2a
AndrewTungYep <andrewtungyep@gmail.com>
2017-03-17 01:38:23 +0000

    Add files via upload
    Variable and small functional modifications are needed to run all the experiments in the report

---
+++ FD simulation TIMER.R
@@ -0,0 +1,62 @@
+initial.pop<-1000
+gen.number<-400
+r.num<-64000
+g.num<-80000
+g.prod<-1000
+p.change<-0.005
+partition<-0.013
+
+#initial population setup
+pop.table<-as.data.frame(matrix(,initial.pop,7))
+colnames(pop.table)<-c("green","red","ratio","division time","divisions","bacteria number","time since division")
+pop.table[,1]<-rpois(initial.pop,g.num)
+pop.table[,2]<-rpois(initial.pop,r.num)
+pop.table[,3]<-pop.table[,2]/pop.table[,1]
+pop.table[,4]<- #setup of initial populations and persistors
+  #sample(1:50,initial.pop,TRUE)  #change for different division time functions
+  runif(initial.pop,56,600)
+pop.table[,5]<-0
+pop.table[,6]<-c(1:initial.pop)
+pop.table[,7]<-as.integer(pop.table[,4]*runif(initial.pop,0,1))
+
+#generation history setup
+history<- data.frame(matrix(,dim(pop.table)[1],8))
+colnames(history)<-c("green","red","ratio","persistor?","divisions","bacteria number","time since division","timestep number")
+history[1:dim(pop.table)[1],1:7]<-pop.table
+history[,8]<-1 #recording inital generation in history data frame
+
+#loop representing a discrete timestep
+for (j in 1:gen.number) { 
+  f<-rbinom(dim(pop.table)[1],pop.table[,1],p.change)
+  pop.table[,1]<-pop.table[,1]+rpois(dim(pop.table)[1],g.prod)-f
+  pop.table[,2]<-pop.table[,2]+f
+  pop.table[,7]<-pop.table[,7]+1
+  dividers<-pop.table[pop.table[,7]>=pop.table[,4],]  #data frame for bacteria which will divide
+  
+#operations for dividing bacteria
+  if (dim(dividers)[1]>0) {
+    next.gen<-data.frame(matrix(,dim(dividers)[1]+dim(pop.table)[1],7))
+    dividers[,8]<-rnorm(dim(dividers)[1],0.5,partition)
+    dividers[,9]<-rbinom(dim(dividers)[1],dividers[,1],dividers[,8])
+    dividers[,10]<-rbinom(dim(dividers)[1],dividers[,2],dividers[,8])
+    #assigning red and green values for the next generation
+    next.gen[1:(dim(dividers)[1]*2),1:2]<-matrix(c(dividers[,9],(dividers[,1]-dividers[,9]),
+                                                   dividers[,10],(dividers[,2]-dividers[,10])),(dim(dividers)[1]*2),2,byrow=FALSE) 
+    next.gen[1:(dim(dividers)[1]*2),3]<-next.gen[1:(dim(dividers)[1]*2),2]/next.gen[1:(dim(dividers)[1]*2),1]
+    next.gen[1:(dim(dividers)[1]*2),4]<-rep(dividers[1:(dim(dividers)[1]),4],2)
+    next.gen[1:(dim(dividers)[1]*2),6]<-rep(dividers[1:(dim(dividers)[1]),6],2)
+    next.gen[1:(dim(dividers)[1]*2),5]<-rep((dividers[1:(dim(dividers)[1]),5]+1),2)
+    next.gen[1:(dim(dividers)[1]*2),7]<-0
+    next.gen[(dim(dividers)[1]*2+1):(dim(dividers)[1]+dim(pop.table)[1]),c(1:7)]<-pop.table[pop.table[,7]<pop.table[,4],c(1:7)]
+    pop.table<-next.gen
+  }
+
+#updating of history table
+  colnames(pop.table)<-c("green","red","ratio","persistor?","divisions","bacteria number","time since division")
+  pop.table[,3]<-pop.table[,2]/pop.table[,1]
+  h<-history
+  history<-data.frame(matrix(,(dim(history)[1]+dim(pop.table)[1]),8))
+  history[1:length(h[,1]),1:8]<-h
+  history[(length(h[,1])+1):length(history[,1]),1:7]<-pop.table
+  history[(length(h[,1])+1):length(history[,1]),8]<-j+1 #updating history with the new generation
+}
